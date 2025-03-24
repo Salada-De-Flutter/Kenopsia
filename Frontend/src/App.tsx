2a69cd23247af3c81c95fbe9aa9033747769331f
Hysteria717 <Fluttered002@gmail.com>
2025-03-24 12:40:59 -0300

    ou vai ou racha

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 import { Layout } from './components';
 import { AlertProvider } from './context/AlertContext';
 import './index.css'; // IMPORTANTE: Importa suas classes
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 function App() {
   return (
     <div className="responsive-container">
