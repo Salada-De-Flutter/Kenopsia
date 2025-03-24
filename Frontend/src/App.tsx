e1fc865a56a5958b62af2ba930d26525bf41e41e
Hysteria717 <Fluttered002@gmail.com>
2025-03-24 03:05:55 -0300

    hehe

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,10 +3,11 @@ import { Router } from './router';
 import './App.css';
 import { Layout } from './components';
 import { AlertProvider } from './context/AlertContext';
+import './index.css'; // IMPORTANTE: Importa suas classes
 
 function App() {
   return (
-    <div className="container">
+    <div className="responsive-container">
     <AlertProvider>
       <Layout>
         <Router />
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css'; // IMPORTANTE: Importa suas classes
 
 createRoot(document.getElementById('root')!).render(
+  <div className="responsive-container">
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
+  </div>
 );
